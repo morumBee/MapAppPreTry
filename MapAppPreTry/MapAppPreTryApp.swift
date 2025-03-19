b955d3f5e14ff083885a23ca50c0c4d88956b38a
MorumBee <zun0323@gmail.com>
2025-03-19 16:52:16 -0300

    Initial Commit

---
+++ MapAppPreTry/MapAppPreTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppPreTryApp.swift
+//  MapAppPreTry
+//
+//  Created by Morumbi on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MapAppPreTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
